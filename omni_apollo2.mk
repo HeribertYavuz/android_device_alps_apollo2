55fb9278d879a3d2655851647a49fbb9f58bb5d4
Yusuf Çağrı Yavuz <44903095+HeribertYavuz@users.noreply.github.com>
2024-08-22 13:44:07 +0300

    Update omni_apollo2.mk

--- omni_apollo2.mk
+++ omni_apollo2.mk
@@ -8,14 +8,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from apollo2 device
 $(call inherit-product, device/alps/apollo2/device.mk)
 
 PRODUCT_DEVICE := apollo2
-PRODUCT_NAME := omni_apollo2
+PRODUCT_NAME := twrp_apollo2
 PRODUCT_BRAND := alps
 PRODUCT_MODEL := apollo2
 PRODUCT_MANUFACTURER := alps
